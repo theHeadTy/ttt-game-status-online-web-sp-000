2d11d40bdd8113804a6ee9ba9d77002b743f4852
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-30 20:43:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -93,7 +93,7 @@ def winner(board)
   combos = WIN_COMBINATIONS
   turns = turn_arr(board)
   x = 0
-  combos.each_with_index do |c, ci|
+  combos.select do |c|
     if (c & turns[:x]).length == 3
       break 'X'
     elsif (c & turns[:o]).length == 3
